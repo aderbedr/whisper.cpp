2dfd08dfaad008a2bf143aaa6c3f2a842211710f
Amanda Der Bedrosian <yellowsmiley7@yahoo.com>
2024-09-26 11:20:12 -0700

    Adding in a way to retrieve the detected language for golang bindings
    Adding in a function, GetDetectedLanguage, which will retrieve the detected language, if available.

--- bindings/go/pkg/whisper/context.go
+++ bindings/go/pkg/whisper/context.go
@@ -355,6 +355,10 @@ func (context *context) IsLANG(t Token, lang string) bool {
 	}
 }
 
+func (context *context) GetDetectedLanguage() string {
+       return whisper.Whisper_lang_str(context.model.ctx.Whisper_full_lang_id())
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 // PRIVATE METHODS
 
